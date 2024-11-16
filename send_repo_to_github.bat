97e08bb4b92699a5802ac11470564dce6ba978d8
pcbproj <pcbproj@yandex.ru>
2024-11-16 17:30:33 +0300

    scale allmost ready. Logo need to add and contacts

--- send_repo_to_github.bat
+++ send_repo_to_github.bat
@@ -1,4 +1,4 @@
-git remote add origin https://github.com/pcbproj/SD_card_test_fatfs.git
+git remote add origin https://github.com/pcbproj/Souveneers.git
 git branch -M main
 git push -u origin main
 pause
